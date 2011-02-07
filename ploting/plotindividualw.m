7c573b8c1d22157e2678a3a520b2573ac6329895
Ondrej on Dice <s0880377@utah.inf.ed.ac.uk>
2011-02-07 18:18:06 +0000

    Changes for some plotting functions.

--- ploting/plotindividualw.m
+++ ploting/plotindividualw.m
@@ -1,10 +1,20 @@
-function plotindividualw(w,peval,savethis)
-% plotindividualw(w,peval,savethis)
+function plotindividualw(w,peval,savethis,localize)
+% plotindividualw(w,peval,savethis, localize)
+if ~exist('localize','var')
+    localize = 0;
+end
 
+[x_mu, y_mu sig] = localizew(w,peval);
 wr=reshape(w, peval.nx, peval.ny, peval.ncomp);
 
 for ii=1:peval.ncomp
     dipshow(wr(:,:,ii));
+    if localize
+        hold on
+        scatter(x_mu(ii)-1, y_mu(ii)-1,'r')
+        drawpixels([peval.nx, peval.ny],':r',-0.5)
+        hold off
+    end
     if savethis
         SaveImageFULL([peval.res_dir '/w' num2str(ii)],'e');
     end
--- ploting/plotresandsum.m
+++ ploting/plotresandsum.m
@@ -1,10 +1,16 @@
-function plotresandsum(dpixc,w,peval,savethis,p)
-% plotresandsum(dpixc,w,peval,savethis,p)
+function plotresandsum(dpixc,w,peval,savethis,p,shownumbers)
+% plotresandsum(dpixc,w,peval,savethis,p,shownumbers)
+if ~exist('shownumbers','var')
+    shownumbers=0;
+end
 
 dipshow(mean(dpixc,3))
 hold on
 [x_mu, y_mu sig] = localizew(w,peval);
 scatter(x_mu-1, y_mu-1,'r')
+if shownumbers
+    text(x_mu+.2-1, y_mu+.2-1, num2cell([1:length(x_mu)]) )
+end
 if exist('p','var')
     scatter(p.x_vec, p.y_vec,'xb')
 end
