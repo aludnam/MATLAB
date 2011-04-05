c0a56bf132431d054ca38ae73ffe76c0c8f4d9e0
Ondrej Mandula <aludnam@gmail.com>
2011-04-05 18:19:15 +0100

    Now: Gradchek agrees for maxdiagno.m and gradmaxdiagno.m.(including determinants of the matrix) Working for different delayed correlation matrices C!

--- conjgradfunctions/gradmaxdiag.m
+++ conjgradfunctions/gradmaxdiag.m
@@ -1,6 +1,6 @@
 function gf=gradmaxdiag(x,varargin)
-% Maximizaton of the diagonality of hte matrix. 
-covmat = varargin{1};   % #pix X #pix X #taus 
+% Maximizaton of the diagonality of hte matrix.
+covmat = varargin{1};   % #pix X #pix X #taus
 sizevec = varargin{2};
 W = reshape(x, sizevec(1)*sizevec(2), sizevec(3));
 ntau = size(covmat,3); % #taus (different time delays)
@@ -13,8 +13,8 @@ for tau=1:ntau
         fcol(col) = (wcol'*covmattmp*wcol);
     end
     Q = diag(1./fcol);
-%     Q = diag(1./diag(W'*covmattmp*W));
-% gftmp(tau,:) = reshape((Q*W'*0.5*(covmattmp+covmattmp'))',1,prod(sizevec));
-gftmp(tau,:) = reshape((Q*W'*0.5*(covmattmp+covmattmp'))',1,prod(sizevec));
+    gftmp(tau,:) = reshape((Q*W'*0.5*(covmattmp+covmattmp'))',1,prod(sizevec));
+    
+    
 end
 gf = sum(gftmp,1);
--- conjgradfunctions/gradmaxdiagno.m
+++ conjgradfunctions/gradmaxdiagno.m
@@ -6,11 +6,12 @@ W = reshape(x, sizevec(1)*sizevec(2), sizevec(3));
 ntau = size(covmat,3); % #taus (different time delays)
 gftmp = zeros(ntau,prod(sizevec));
 for tau=1:ntau
-    covmattmp = covmat(:,:,tau);
-    P = W'*covmattmp*W;
+    C = covmat(:,:,tau);
+    P = W'*C*W;
     Q = diag(1./diag(P));
     R = inv(P);
-    gfr = (Q'*W' - (W*R)')*0.5*(covmattmp+covmattmp');
+%     gfr = (Q'*W' - (W*R)')*0.5*(C+C');
+    gfr = Q'*W'*0.5*(C+C') - 0.5*((C*W*R)'+R*W'*C);
     gftmp(tau,:) = reshape(gfr',1,prod(sizevec));
 end
 
